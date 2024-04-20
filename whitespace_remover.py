b56631baf97c4176676f83e58fb90a76c29c4285
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2024-04-20 17:06:51 +0200

    20.04.23 simple whitespace remover

---
+++ whitespace_remover.py
@@ -0,0 +1,15 @@
+import os
+
+path = 'C:\\PATH\\TO\\DIRECTORY\\FOLDER\\FOLDER\\'
+
+
+def replace_whitespace_with_underscore(directory):
+    folders = [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
+
+    for folder in folders:
+        old_name = os.path.join(directory, folder)
+        new_name = os.path.join(directory, folder.replace(' ', '_'))
+        os.rename(old_name, new_name)
+
+
+replace_whitespace_with_underscore(path)
