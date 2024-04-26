fbf1bf118346754bf6dbda969bea056719dacf1e
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2024-04-26 21:05:12 +0200

    26.04.23 added outputFolder

--- zipper.py
+++ zipper.py
@@ -3,14 +3,18 @@ import zipfile
 
 # zippt alle files in einem folder
 
-path = 'D:\\The\\Path\\to\\your\\folder\\Zip'
+path = 'D:\\The\\Path\\to\\your\\input_folder\\Zip'
+zipPath = 'D:\\The\\Path\\to\\your\\output_folder\\Zip'
 padding = '_YOUR_PADDING'
+
+if not os.path.exists(zipPath):
+    os.makedirs(zipPath)
 
 print("Starting loop")
 for foldername in os.listdir(path):
     if os.path.isdir(os.path.join(path, foldername)):
         zipname = foldername + padding + '.zip'
-        with zipfile.ZipFile(os.path.join(path, zipname), 'w', zipfile.ZIP_DEFLATED) as zipf:
+        with zipfile.ZipFile(os.path.join(zipPath, zipname), 'w', zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk(os.path.join(path, foldername)):
                 for file in files:
                     zipf.write(os.path.join(root, file), os.path.join(foldername, file))
